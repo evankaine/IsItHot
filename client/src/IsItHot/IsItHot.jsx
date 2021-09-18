0bc5056e6e4e2abe125ee945f11161441842359b
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-09-18 12:27:50 -0400

    trying to resolve input value empty issue

--- client/src/IsItHot/IsItHot.jsx
+++ client/src/IsItHot/IsItHot.jsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
 import IsItHotContent from './IsItHotContent';
-import Form from './Form';
 import "./IsItHot.css"
 import axios from "axios"
-
 
 const OPENWEATHER_KEY = process.env.REACT_APP_API_KEY;
 
@@ -11,32 +9,27 @@ const URL = `https://api.openweathermap.org/data/2.5`
 
 // /weather?q=new%20york&units=imperial&appid=${OPENWEATHER_KEY}
 
-
 export default function IsItHot(props) {
 
-  const [weatherData, setWeatherData] = useState([]);
   const [formInput, setFormInput] = useState('')
-  // const { formInput } = props
+  const [weatherData, setWeatherData] = useState([]);
 
-  const submitForm = (e) => {
-    e.preventDefault()
-    setFormInput()
-  }
-  console.log(formInput)
+
   useEffect(() => {
-    fetchData();
+    handleInput();
   }, []);
-  const fetchData = async () => {
-    const res = await axios.get(`${URL}/weather?q=${formInput}&units=imperial&appid=${OPENWEATHER_KEY}`);
+  const handleInput = async () => {
+    const res = await axios.get(`${URL}/weather?q=miami&units=imperial&appid=${OPENWEATHER_KEY}`);
     setWeatherData(res.data.main);
+    console.log(formInput)
   };
-  
+
   return (
     <div className="hot-wrapper">
       <h1>Is It Hot ?</h1>
-      <form onSubmit={submitForm}>
-        <input></input>
-        <button>Is It Hot</button>
+      <form>
+        <input placeholder="City" onChange={e => setFormInput(e.target.value)}></input>
+        <button onClick={() => handleInput()}>Is It Hot</button>
       </form>
       <IsItHotContent weatherData={weatherData}/>
     </div>
